ad58842b3597abb36d230a6e7c1b3566644d1dff
Peter Pistorius <peter.pistorius@gmail.com>
2018-07-26 12:42:35 +0200

    Add new rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,37 @@
 module.exports = {
-  parser: "babel-eslint",
-  extends: ["eslint-config-airbnb", "eslint-config-standard"],
+  parser: 'babel-eslint',
+  extends: ['eslint-config-airbnb', 'eslint-config-standard'],
   env: {
-    jest: true
+    jest: true,
   },
   globals: {
-    fetch: true
+    fetch: true,
   },
   rules: {
-    "no-console": 0,
-    "new-cap": 0,
-    "arrow-body-style": 0,
-    "comma-dangle": ["error", "always-multiline"],
-    "arrow-parens": ["error", "as-needed"],
-    "import/no-extraneous-dependencies:": 0,
-    "import/no-unresolved": 0,
-    "import/extensions": 0,
-    "import/prefer-default-export": 0,
-    "no-use-before-define": 0,
-    "react/jsx-filename-extension": 0,
-    "react/prefer-stateless-function": 0,
-    "react/forbid-prop-types": 0,
-    "react/no-unescaped-entities": 0,
-    "react/require-default-props": 0,
-    "react/sort-comp": 0,
-    "react/prop-types": [
-      "error",
+    'no-console': 0,
+    'new-cap': 0,
+    'arrow-body-style': 0,
+    'comma-dangle': ['error', 'always-multiline'],
+    'arrow-parens': ['error', 'as-needed'],
+    'import/no-extraneous-dependencies': 0,
+    'object-curly-newline': 0,
+    'function-paren-newline': 0,
+    'prefer-destructuring': 0,
+    'import/no-unresolved': 0,
+    'import/extensions': 0,
+    'import/prefer-default-export': 0,
+    'no-use-before-define': 0,
+    'react/jsx-filename-extension': 0,
+    'react/prefer-stateless-function': 0,
+    'react/forbid-prop-types': 0,
+    'react/no-unescaped-entities': 0,
+    'react/require-default-props': 0,
+    'react/sort-comp': 0,
+    'react/prop-types': [
+      'error',
       {
-        ignore: ["style", "children"]
-      }
-    ]
-  }
-};
+        ignore: ['style', 'children'],
+      },
+    ],
+  },
+}
